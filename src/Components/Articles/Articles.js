cb769ccea6b42c729cf5ebb9cfd0ee1bd314be7a
Francesca McConnell <fmcconnell59@gmail.com>
2022-06-07 16:48:34 -0600

    Map through articles to return tiles

--- src/Components/Articles/Articles.js
+++ src/Components/Articles/Articles.js
@@ -2,26 +2,27 @@ import './Articles.css'
 import ArticleTile from '../ArticleTile/ArticleTile'
 
 const Articles = ({ articles }) => {
-  const tiles = () => {
-    let counter = 0
-    articles.map((article) => {
-      counter += 1
-      return (
-        <ArticleTile
-          key={counter}
-          genre={article.section}
-          title={article.title}
-          abstract={article.abstract}
-          url={article.url}
-          byline={article.byline}
-          published={article.published_date}
-          image={article.multimedia.url}
-          alt={article.multimedia.caption}
-        />
-      )
-    })
-  }
-  return <section className='articles-section'>{tiles}</section>
+  let counter = 0
+
+  const tiles = articles.map((article) => (
+    <ArticleTile
+      key={counter++}
+      genre={article.section}
+      title={article.title}
+      abstract={article.abstract}
+      url={article.url}
+      byline={article.byline}
+      published={article.published_date}
+      image={article.multimedia[0].url}
+      alt={article.multimedia[0].caption}
+    />
+  ))
+
+  return (
+    <section className='articles-section'>
+      <div className='tiles-container'>{tiles}</div>
+    </section>
+  )
 }
 
 export default Articles
