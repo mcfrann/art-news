a620dea6d41696c513a568d297eda2dc153a5d26
Francesca McConnell <fmcconnell59@gmail.com>
2022-06-07 16:48:47 -0600

    Add tile format

--- src/Components/ArticleTile/ArticleTile.js
+++ src/Components/ArticleTile/ArticleTile.js
@@ -1,7 +1,34 @@
-import './ArticleTile'
+import './ArticleTile.css'
 
-const ArticleTile = () => {
-  return <h1>Article Tile</h1>
+const ArticleTile = ({
+  key,
+  genre,
+  title,
+  abstract,
+  url,
+  byline,
+  published,
+  image,
+  alt
+}) => {
+  return (
+    <section
+      src={image}
+      className='article-tile'
+      id={key}
+      style={{
+        backgroundImage: `url(${image})`,
+        // height: '100%',
+        // width: '100%',
+        objectFit: 'cover'
+      }}
+    >
+      <div className='tile-content'>
+        <h2 className='article-title'>{title}</h2>
+        <p className='article-byline'>{byline}</p>
+      </div>
+    </section>
+  )
 }
 
 export default ArticleTile
