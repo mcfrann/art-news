4cc319fc98d05547f80ff8857f5a39413bf17c5f
Francesca McConnell <fmcconnell59@gmail.com>
2022-06-11 15:45:01 -0600

    Change footer link

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,13 +4,13 @@ const Footer = () => {
   return (
     <footer>
       <div className='resources'>
-        <p>
+        <p className='credit'>
           Site inspiration from{' '}
           <a href='https://criticalmass.com/work' target='_blank'>
             <b>Critical Mass</b>
           </a>
         </p>
-        <p>
+        <p className='credit'>
           Read more articles at{' '}
           <a
             href='https://www.nytimes.com/subscription?campaignId=8HHWL&ds_c=71700000096479712&gclid=Cj0KCQjw-pCVBhCFARIsAGMxhAfNEDxpx2Up_BNQV63jx_75wvaKg6jM5JlYhEo5GEfcV8hOpvFR6mYaAt-hEALw_wcB&gclsrc=aw.ds'
@@ -22,10 +22,10 @@ const Footer = () => {
       </div>
       <div className='site-info'>
         <p>
-          This site was built by{' '}
+          Site built by{' '}
           <a
             className='github-link'
-            href='https://github.com/mcfrann'
+            href='https://github.com/mcfrann/art-news'
             target='_blank'
           >
             <b>Francesca McConnell</b>
