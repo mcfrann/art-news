cabb9d2de2814139567211972c9e042d68ae0ec8
Francesca McConnell <fmcconnell59@gmail.com>
2022-06-11 14:09:29 -0600

    Add more structure to ArticlePage

--- src/Components/ArticlePage/ArticlePage.js
+++ src/Components/ArticlePage/ArticlePage.js
@@ -1,7 +1,19 @@
 import './ArticlePage.css'
 
-const ArticlePage = () => {
-  return <h1>Main article</h1>
+const ArticlePage = ({ currentArticle }) => {
+  return (
+    <section className='selected-article'>
+      <h2>{currentArticle.title}</h2>
+      <img
+        src={currentArticle.multimedia[0].url}
+        alt={currentArticle.multimedia[0].copyright}
+      />
+      <p className='abstract'>{currentArticle.abstract}</p>
+      <p className='nyt-link'>
+        Read the full article here: {currentArticle.url}
+      </p>
+    </section>
+  )
 }
 
 export default ArticlePage
