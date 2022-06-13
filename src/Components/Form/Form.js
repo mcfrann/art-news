c7b76fae52632c7e973f534eb332f7b9b56b9e4b
Francesca McConnell <fmcconnell59@gmail.com>
2022-06-12 22:53:03 -0600

    Import search icon

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,38 +1,42 @@
 import './Form.css'
+import search from '../../images/search.png'
 
 const Form = ({ articles, setFiltered, input, setInput }) => {
   const handleClick = (e) => {
     e.preventDefault()
-    if (input != 'All') {
-      const filtered = articles.filter(
-        (article) => article.section === input.toLowerCase()
-      )
-      setFiltered(filtered)
-    }
+    const filtered = articles.filter(
+      (article) => article.section === input.toLowerCase()
+    )
+    setFiltered(filtered)
     setInput('')
   }
 
   return (
     <form>
-      <div className='inputs-wrapper'>
-        <div className='single-input'>
-          <label htmlFor='genre-select'>Genre:</label>
-          <select
-            id='genre-select'
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          >
-            <option value='All'>All</option>
-            <option value='Arts'>Arts</option>
-            <option value='Theater'>Theater</option>
-            <option value='Movies'>Movies</option>
-            <option value='Style'>Style</option>
-            <option value='Podcasts'>Podcasts</option>
-            <option value='Books'>Books</option>
-          </select>
-        </div>
+      <div className='dropdown'>
+        <label htmlFor='select'>Filter by: </label>
+        <select
+          id='select'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        >
+          <option value='' disabled>
+            Choose
+          </option>
+          <option value='Arts'>Arts</option>
+          <option value='Theater'>Theater</option>
+          <option value='Movies'>Movies</option>
+          <option value='Style'>Style</option>
+          <option value='Podcasts'>Podcasts</option>
+          <option value='Books'>Books</option>
+        </select>
+        <img
+          className='search-icon'
+          src={search}
+          alt='search icon'
+          onClick={(e) => handleClick(e)}
+        />
       </div>
-      <button onClick={(e) => handleClick(e)}>Filter</button>
     </form>
   )
 }
