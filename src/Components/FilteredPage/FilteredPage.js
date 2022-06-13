90ee40dddcef1c21e66e12afd6b95ae5d73098bf
Francesca McConnell <fmcconnell59@gmail.com>
2022-06-12 22:52:24 -0600

    Delete filteredPage component

--- src/Components/FilteredPage/FilteredPage.js
+++
@@ -1,8 +0,0 @@
-import './FilteredPage.css'
-import { useEffect } from 'react'
-
-const FilteredPage = ({ filtered }) => {
-  return <div className='filtered-page'></div>
-}
-
-export default FilteredPage
